c98e144d1bf2fd455fef81c9d5ab404c079fe815
Maksym Mykhailenko <maxceem@gmail.com>
2021-03-12 13:19:42 +0200

    fix: remove taas events payload validation

--- consumer/src/processors/TaaSResourceBooking/ResourceBookingUpdated.js
+++ consumer/src/processors/TaaSResourceBooking/ResourceBookingUpdated.js
@@ -3,38 +3,19 @@
  */
 import _ from 'lodash';
 const co = require('co');
-import Joi from 'joi';
 import SalesforceEventService from '../../services/SalesforceEventService';
 
-ResourceBookingUpdatedSchema = Joi.object().keys({
-    currentUser: Joi.object().required(),
-    id: Joi.string().uuid().required(),
-    data: Joi.object().keys({
-      status: Joi.jobStatus(),
-      startDate: Joi.date().allow(null),
-      endDate: Joi.date().allow(null),
-      memberRate: Joi.number().allow(null),
-      customerRate: Joi.number().allow(null),
-      rateType: Joi.rateType()
-    }).required()
-  }).required()
-
-
 /**
- * Handle Kafka JSON message of challenge created.
+ * Handle Kafka JSON message of TaaS Resource Booking updated.
  *
  * @param {Object} message the Kafka JSON message
  * @param {Object} ruleSets
  *
- * @return {Promise} promise resolved to notifications
+ * @return {Promise}
  */
 const handle = (logger, message, ruleSets) => co(function* () {
-  const payload = message.payload;
-  const validatedPayload = ResourceBookingUpdatedSchema.validate(payload);
-  logger.info('validatedPayload=>' + JSON.stringify(validatedPayload));
-  if (!validatedPayload.error) {
-    SalesforceEventService.postEvent(logger, message);
-  }
+  logger.info(message);
+  SalesforceEventService.postEvent(logger, message);
   return {};
 });
 
